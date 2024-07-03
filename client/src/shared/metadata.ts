bd03d9fcad94d8d9fba232821991666c8be80350
Erlon R. Cruz <erlon@canonical.com>
2024-07-03 16:41:54 -0300

    Bump Serenade Version

--- client/src/shared/metadata.ts
+++ client/src/shared/metadata.ts
@@ -1,7 +1,7 @@
 import * as os from "os";
 
 export default class Metadata {
-  version = "2.0.2";
+  version = "2.0.2-community-1.0.beta";
 
   identifier(application: string, language: any): string {
     return JSON.stringify({
